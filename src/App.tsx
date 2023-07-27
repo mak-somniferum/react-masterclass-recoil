fba580146d89535525f5450ec37b5cfe7a7ab6a9
mak-somniferum <suhjin700@gmail.com>
2023-07-27 09:12:24 +0900

    practice 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { hourSelector, minuteState } from './atoms';
 
 function App() {
-  return <></>;
+  const [minutes, setMinutes] = useRecoilState(minuteState);
+  const hours = useRecoilValue(hourSelector);
+  const onMinutesChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setMinutes(+event.currentTarget.value);
+  };
+  return (
+    <>
+      <input value={minutes} onChange={onMinutesChange} type="number" placeholder="Minutes" />
+      <input value={hours} type="number" placeholder="Hours" />
+    </>
+  );
 }
 
 export default App;
--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,1 +1,13 @@
 import { atom, selector } from 'recoil';
+
+export const minuteState = atom({
+  key: 'minutes',
+  default: 0,
+});
+
+export const hourSelector = selector({
+  key: 'hours',
+  get: ({ get }) => {
+    return get(minuteState) / 60;
+  },
+});
